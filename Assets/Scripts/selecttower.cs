58ef1fe5f13964ef5ed770f6f3e3f0a1d19244f2
Y009 <henryj654@gmail.com>
2016-04-11 20:06:39 +0300

    towerid on selectitavad
    toweri listi sort vaja fixida; saab hakata toweri upgrade asju lisama hakata

---
+++ Assets/Scripts/selecttower.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selecttower : MonoBehaviour {
+
+    private float dist = 40;
+    public LayerMask towerMask;
+    private GameObject tower;
+
+	// Use this for initialization
+	void Start () {
+        tower = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, dist, towerMask))     //kui raycast saab towerile pihta
+            {
+                if (tower != null)  
+                    tower.GetComponent<bullet1>().upgrading = false;  //kui ei ole esimene tower, siis annan eelmisele teada, et ei upgradei enam
+                tower = hit.transform.parent.gameObject;            //panen toweri uueks agmeobjectiks
+                tower.GetComponent<bullet1>().upgrading = true;     //anna teada towerile, et kasutab seda
+            }
+        }
+	}
+}
